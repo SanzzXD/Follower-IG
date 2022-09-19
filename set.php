d5e7df6cc696192456eadd01e0ee2b5050630cfa
SanzzXD <112183374+SanzzXD@users.noreply.github.com>
2022-09-19 20:36:01 +0700

    Add files via upload

---
+++ set.php
@@ -0,0 +1,23 @@
+<?php
+///////////////////////////////////////////////////////
+///////// KAGAK USAH DELET CREDIT AJG /////////////////
+///////////////////////////////////////////////////////
+
+# Creator: Clownxx
+# youtube: Clownxx
+# Group Telegram: t.me/ofclownxx
+# Group Whatsapp: https://chat.whatsapp.com/IhHg1S2lJCsIdf5NeasbMt
+# Owner site: myclownxx.blogstop.com
+
+
+# Masukan username akun ori/asli
+# Enter the original account username
+$user="sanzzwibutzy_";
+
+# Username akun tumbal
+# Enter the username of the account that was sacrificed
+$t_user="osprint_niki";
+
+# Passwoard/sandi akun tumbal
+# enter the password of the account that was sacrificed
+$t_pass="osprint";
